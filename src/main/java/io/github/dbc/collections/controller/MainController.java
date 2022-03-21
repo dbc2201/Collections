8937326bf18383d5bc606038a79d7839ae9fc490
dbc2201 <divyanshb22@gmail.com>
2022-03-21 17:18:54 +0530

    open create view from button

--- src/main/java/io/github/dbc/collections/controller/MainController.java
+++ src/main/java/io/github/dbc/collections/controller/MainController.java
@@ -1,9 +1,15 @@
 package io.github.dbc.collections.controller;
 
+import io.github.dbc.collections.CRUDApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
@@ -22,8 +28,13 @@ public class MainController {
     private Label welcomeText;
 
     @FXML
-    public void createNewStudent(ActionEvent actionEvent) {
-
+    public void createNewStudent(ActionEvent actionEvent) throws IOException {
+        Stage createStudentStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(CRUDApplication.class.getResource("create-view.fxml"));
+        Scene scene = new Scene(loader.load(), 400.0, 400.0);
+        createStudentStage.setTitle("Create New Student");
+        createStudentStage.setScene(scene);
+        createStudentStage.show();
     }
 
     @FXML
