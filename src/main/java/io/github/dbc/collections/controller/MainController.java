f6ed7b1e5bb221a8d35774c4516613e644f7c609
dbc2201 <divyanshb22@gmail.com>
2022-03-21 17:15:50 +0530

    create methods and buttons for the main view

--- src/main/java/io/github/dbc/collections/controller/MainController.java
+++ src/main/java/io/github/dbc/collections/controller/MainController.java
@@ -1,11 +1,44 @@
 package io.github.dbc.collections.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
+    public Button createNewStudentButton;
+
+    @FXML
+    public Button viewAllStudentsButton;
+
+    @FXML
+    public Button updateStudentInfoButton;
+
+    @FXML
+    public Button deleteStudentButton;
+
+    @FXML
     private Label welcomeText;
 
+    @FXML
+    public void createNewStudent(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void viewAllStudents(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void updateStudentInfo(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void deleteStudent(ActionEvent actionEvent) {
+
+    }
 
 }
