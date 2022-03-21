d0bdede0a4c7b772fa6de5f09337028719c10a36
dbc2201 <divyanshb22@gmail.com>
2022-03-21 18:13:08 +0530

    add code to create a new student in the list

--- src/main/java/io/github/dbc/collections/controller/StudentController.java
+++ src/main/java/io/github/dbc/collections/controller/StudentController.java
@@ -3,17 +3,55 @@ package io.github.dbc.collections.controller;
 import io.github.dbc.collections.model.Student;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentController {
 
+    @FXML
+    public TextField createFirstNameTextField;
+    @FXML
+    public TextField createLastNameTextField;
+
+    @FXML
+    public Button createNewStudentButton;
+
     private List<Student> studentList;
+
+    public StudentController() {
+        this.studentList = new ArrayList<>();
+    }
 
     // create
     @FXML
     public void createNewStudentInList(ActionEvent actionEvent) {
+        // get the string value for first name, and last name from the text fields
+        String firstName = createFirstNameTextField.getText();
+        String lastName = createLastNameTextField.getText();
 
+        // check if the string is null or empty
+        if (firstName == null || firstName.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setContentText("First Name should not be empty!");
+            alert.showAndWait();
+        } else if (lastName == null || lastName.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setContentText("Last Name should not be empty!");
+            alert.showAndWait();
+        } else {
+            Student student = new Student(firstName, lastName);
+            studentList.add(student);
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("SUCCESS");
+            alert.setContentText(firstName + " added to the list!");
+            alert.show();
+        }
     }
 
     // read
