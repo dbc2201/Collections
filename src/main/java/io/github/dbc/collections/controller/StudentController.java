42d75f887a20841982eb80f098f233756854bf34
dbc2201 <divyanshb22@gmail.com>
2022-03-21 18:01:27 +0530

    add methods to crud student list, and a list to store in-memory objects

--- src/main/java/io/github/dbc/collections/controller/StudentController.java
+++ src/main/java/io/github/dbc/collections/controller/StudentController.java
@@ -1,5 +1,36 @@
 package io.github.dbc.collections.controller;
 
+import io.github.dbc.collections.model.Student;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.util.List;
+
 public class StudentController {
 
+    private List<Student> studentList;
+
+    // create
+    @FXML
+    public void createNewStudentInList(ActionEvent actionEvent) {
+
+    }
+
+    // read
+    @FXML
+    public void readAllStudentsFromList(ActionEvent actionEvent) {
+
+    }
+
+    // update
+    @FXML
+    public void updateStudentDetailsInList(ActionEvent actionEvent) {
+
+    }
+
+    // delete
+    @FXML
+    public void deleteStudentFromList(ActionEvent actionEvent) {
+
+    }
 }
